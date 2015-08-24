15019c0831bcf68cc18c2b191895cda26240c53c
Nathan <nathanrbourke@gmail.com>
2015-08-24 17:53:55 -0400

    Build UsersController#update/#edit, Build edit view, dump user notifications/errors into partial.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,9 +7,24 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      flash[:success] = "#{@user.username} created sucessfully!"
       redirect_to new_user_path
     else
       render :new
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "#{@user.username} updated sucessfully!"
+      redirect_to edit_user_path
+    else
+      render :edit
     end
   end
 
